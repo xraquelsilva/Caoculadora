01c796068bfb3b93ab380d4b65bf831620b54e81
raquelsilvax <raquelsilvadosanto@hotmail.com>
2024-05-07 16:11:10 -0300

    Adiciona arquivos do projeto

---
+++ caoculadora/caoculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  caoculadora
+//
+//  Created by Raquel Silva dos Santos on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Cachorrinho")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ caoculadora/caoculadora/caoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  caoculadoraApp.swift
+//  caoculadora
+//
+//  Created by Raquel Silva dos Santos on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct caoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
