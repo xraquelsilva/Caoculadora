4bd92bf34edaa52272905b8bd85afe0866ccd170
raquelsilvax <raquelsilvadosanto@hotmail.com>
2024-05-08 13:37:39 -0300

    Insere textfields

--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var years: Int? = nil
+    @State var months: Int? = nil
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Cachorrinho")
+        VStack(alignment: .leading) {
+            Text("Qual a idade do seu cão?")
+                .font(.system(size: 24))
+            Text("Anos")
+            TextField("Digite a idade canina",
+                      value: $years,
+                      format: .number)
+            Text("Meses")
+            TextField("Digite os meses",
+                      value: $months,
+                      format: .number)
         }
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.numberPad)
+        .bold()
+        .fontDesign(.rounded)
         .padding()
     }
 }
