ae84fbca5af55c0fa1640f319790dc94d2ebedde
raquelsilvax <raquelsilvadosanto@hotmail.com>
2024-05-09 17:47:40 -0300

    Adiciona segmented control e spacer

--- caoculadora/caoculadora/ContentView.swift
+++ caoculadora/caoculadora/ContentView.swift
@@ -12,12 +12,15 @@ struct ContentView: View {
     @State var years: Int? = nil
     @State var months: Int? = nil
     @State var result: Int?
+    let portes = ["Pequeno", "Médio", "Grande"]
+    @State var porte: String = "Pequeno"
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 5.0) {
             Text("Qual a idade do seu cão?")
                 .font(.system(size: 24))
             Text("Anos")
+            
             TextField("Digite os anos caninos",
                       value: $years,
                       format: .number)
@@ -26,23 +29,36 @@ struct ContentView: View {
                       value: $months,
                       format: .number)
             Text("Porte")
-            //aqui fica segment control
+            
+            
+            Picker("Porte", selection: $porte) {
+                ForEach(portes, id: \.self) { porte in
+                    Text(porte)
+                        .tag(porte)
+                }
+            }
+            .pickerStyle(.segmented)
+            
+            Divider()
+                .background(.green)
+            Spacer()
+            
             if let result {
                 Text("Seu cachorro tem, em idade humana...")
                 Text("\(result) anos")
                     .font(.system(size: 60))
+                    .frame(maxHeight: 150)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
             } else {
                 Image(ImageResource.clarinha)
                     .resizable()
                     .scaledToFit()
                     .frame(maxHeight: 150)
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                .shadow(radius: 20)
+                    .shadow(radius: 20)
             }
-            Button(action: {
-                result = 23
-                //calculo da idade convertida
-            }, label: {
+            Spacer()
+            Button(action: processYears, label: {
                 ZStack {
                     Color.blue
                     Text("Cãocular")
@@ -51,13 +67,29 @@ struct ContentView: View {
                 .cornerRadius(10)
             })
             .frame(height: 50)
-            
         }
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad)
         .bold()
         .fontDesign(.rounded)
         .padding()
+    }
+
+    func processYears() {
+        // idade canina em idade humana = years * 7 + fração de meses do ano * 7
+        guard
+            let years,
+            let months
+        else { // so executa se years nao for nil
+            print("Preencha o campo de entrada")
+            return // deve temrinar a execução da função -> return vazio
+            // se nao escrevi nada em anos, o botao nao fará nada
+        }
+        guard years > 0 || months > 0 else {
+            print("algum campo tem que ter um valor maior que 0")
+            return
+        }
+        result = years * 7 + months*7/12
     }
 }
 
